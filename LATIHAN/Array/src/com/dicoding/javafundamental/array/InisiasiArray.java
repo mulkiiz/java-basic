91b6d0cc659da6f42b455872f7a6d47a732b3d02
mulkiiz <admiportfolio@gmail.com>
2021-07-11 05:29:13 +0700

    update class array: contoh inisial array yang lain

--- LATIHAN/Array/src/com/dicoding/javafundamental/array/InisiasiArray.java
+++ LATIHAN/Array/src/com/dicoding/javafundamental/array/InisiasiArray.java
@@ -6,8 +6,14 @@ public class InisiasiArray {
         arrInt[0] = 6; //contoh pengisian nilai array yang lain
         arrInt[1] = 7;
 
+        int[] arrIntx = {55, 66}; //contoh inisialisasi arr yang lebih sederhana
+
         System.out.println("nilai arrInt index ke-0 = " + arrInt[0]);
-        System.out.println("nilai arrInt index ke-1 = " +arrInt[1]);
-        System.out.println("nilai arrInt index ke-2 = " +arrInt[2]);
+        System.out.println("nilai arrInt index ke-1 = " + arrInt[1]);
+        System.out.println("nilai arrInt index ke-2 = " + arrInt[2]);
+        System.out.println("-----");
+        System.out.println("nilai arrIntx index ke-2 = " + arrIntx[0]);
+        System.out.println("nilai arrIntx index ke-2 = " + arrIntx[1]);
+
     }
 }
