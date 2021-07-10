c6994db5cc95c6b53ce4b8acff562d668ea13e48
mulkiiz <admiportfolio@gmail.com>
2021-07-10 20:16:43 +0700

    update notes.txt, menambah lib jar ke Main.class

--- src/com/dicoding/javafundamental/basic/Main.java
+++ src/com/dicoding/javafundamental/basic/Main.java
@@ -4,6 +4,9 @@ import com.dicoding.javafundamental.basic.kendaraan.Kereta;
 import com.dicoding.javafundamental.basic.kendaraan.Mobil;
 import com.dicoding.javafundamental.basic.kendaraan.Motor;
 import com.dicoding.javafundamental.basic.musik.Gitar;
+import org.apache.commons.lang3.time.DateUtils;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args){
@@ -13,5 +16,10 @@ public class Main {
         Mobil.jumlahBan();
         Motor.jumlahBan();
         Kereta.jumlahBan();
+
+        Date today = new Date();
+        System.out.println("Hari ini : " + today);
+        Date tomorrow = DateUtils.addDays(today,1);
+        System.out.println("Besok : " + tomorrow);
     }
 }
