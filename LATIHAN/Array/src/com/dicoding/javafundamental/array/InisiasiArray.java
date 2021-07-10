54291203e6f1016861f0d9c8774107dc3b653cc7
mulkiiz <admiportfolio@gmail.com>
2021-07-11 05:27:05 +0700

    initial commit: array

---
+++ LATIHAN/Array/src/com/dicoding/javafundamental/array/InisiasiArray.java
@@ -0,0 +1,13 @@
+package com.dicoding.javafundamental.array;
+
+public class InisiasiArray {
+    public static void main(String[] args){
+        int[] arrInt = new int[]{1,2,3,4,5};
+        arrInt[0] = 6; //contoh pengisian nilai array yang lain
+        arrInt[1] = 7;
+
+        System.out.println("nilai arrInt index ke-0 = " + arrInt[0]);
+        System.out.println("nilai arrInt index ke-1 = " +arrInt[1]);
+        System.out.println("nilai arrInt index ke-2 = " +arrInt[2]);
+    }
+}
