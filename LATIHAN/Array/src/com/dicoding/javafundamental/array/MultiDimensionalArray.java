d8fc20ae6fd6a61160ca434d64f241ad63f3ae11
mulkiiz <admiportfolio@gmail.com>
2021-07-11 06:33:05 +0700

    update comment class array multidimensi

--- LATIHAN/Array/src/com/dicoding/javafundamental/array/MultiDimensionalArray.java
+++ LATIHAN/Array/src/com/dicoding/javafundamental/array/MultiDimensionalArray.java
@@ -2,10 +2,10 @@ package com.dicoding.javafundamental.array;
 
 public class MultiDimensionalArray {
     public static void main(String[] args){
-        char[][] arrChar = new char[2][];
+        char[][] arrChar = new char[1][2]; //lagi - lagi penting untuk inisialisasi panjang tipe data di awal
 
-        arrChar[0] = new char[2];
-        arrChar[1] = new char[3];
+        arrChar[0] = new char[2]; //walaupun pada akhirnya panjang tipe datanya berubah
+        arrChar[1] = new char[3]; //namun panjang data di sini tidak boleh melebihi inisialisasi
 
         // Dimensi 1 yang indeksnya 0 memiliki panjang elemen 2
         arrChar[0][0] = 'A';
@@ -19,7 +19,7 @@ public class MultiDimensionalArray {
         // Tampilkan semua data dari dimensi 1 yang memiliki indeks 0
         System.out.println("Dimensi 1 dengan indeks 0 dan Dimensi 2 dengan indeks 0 : " + arrChar[0][0]);
         System.out.println("Dimensi 1 dengan indeks 0 dan Dimensi 2 dengan indeks 1 : " + arrChar[0][1]);
-        
+
         // Tampilkan semua data dari dimensi 1 yang memiliki indeks 1
         System.out.println("Dimensi 1 dengan indeks 1 dan Dimensi 2 dengan indeks 0 : " + arrChar[1][0]);
         System.out.println("Dimensi 1 dengan indeks 1 dan Dimensi 2 dengan indeks 1 : " + arrChar[1][1]);
