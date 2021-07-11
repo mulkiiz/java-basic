4bd69562ca2eb24fb2e90cb260e7dc93b5b3eb86
mulkiiz <admiportfolio@gmail.com>
2021-07-11 09:20:42 +0700

    add new class Hewan, constructor, and 3 method

--- LATIHAN/Obyek/src/com/dicoding/javafundamental/obyek/Main.java
+++ LATIHAN/Obyek/src/com/dicoding/javafundamental/obyek/Main.java
@@ -2,8 +2,15 @@ package com.dicoding.javafundamental.obyek;
 
 public class Main {
     public static void main(String[] args){
-        // menciptakan object dengan nama 'obyekHewan'
-        Hewan obyekhewan = new Hewan();
-        obyekhewan.cetakNama("Elang");
+        Hewan Elang = new Hewan("Elang");
+        Hewan Kucing = new Hewan("Kucing");
+
+        Elang.beratHewan(5);
+        Elang.jumlahKakiHewan(2);
+        Elang.cetakHewan();
+
+        Kucing.beratHewan(2);
+        Kucing.jumlahKakiHewan(4);
+        Kucing.cetakHewan();
     }
 }
