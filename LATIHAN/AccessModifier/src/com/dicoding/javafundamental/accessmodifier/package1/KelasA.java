6d7b3b7de6f505f2de59fe3f517d5791d5992b4d
mulkiiz <admiportfolio@gmail.com>
2021-07-11 11:42:46 +0700

    update main class: tentang modifier

---
+++ LATIHAN/AccessModifier/src/com/dicoding/javafundamental/accessmodifier/package1/KelasA.java
@@ -0,0 +1,18 @@
+package com.dicoding.javafundamental.accessmodifier.package1;
+
+public class KelasA {
+    private int memberA = 5; //tidak bisa diakses dari kelas luar
+
+    char memberB = 'A'; //modifier: default
+    double memberC = 1.5;
+
+    private int functionA() {
+        return memberA;
+    } //tidak bisa diakses dari kelas luar
+
+    int functionB() { //bisa diakses dari kelas luar
+        // Pemanggilan private member dan private function
+        int hasil = functionA() + memberA;
+        return hasil;
+    }
+}
