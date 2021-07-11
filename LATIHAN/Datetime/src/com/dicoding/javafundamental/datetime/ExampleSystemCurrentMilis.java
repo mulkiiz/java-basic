e03fbb32712cd320d8452b702355fa16b762fd85
mulkiiz <admiportfolio@gmail.com>
2021-07-11 21:46:05 +0700

    update notes, add new project Datetime

---
+++ LATIHAN/Datetime/src/com/dicoding/javafundamental/datetime/ExampleSystemCurrentMilis.java
@@ -0,0 +1,13 @@
+package com.dicoding.javafundamental.datetime;
+
+import java.util.Date;
+
+public class ExampleSystemCurrentMilis {
+    public static void main(String[] args) {
+        long timeNow = System.currentTimeMillis();
+        System.out.println("Waktu sekarang adalah " + timeNow + " milisecond");
+
+        Date date = new Date();
+        System.out.println("Tanggal sekarang adalah " + date.toString());
+    }
+}
